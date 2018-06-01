fd080cbd0889fc704b07d2a52089afa0760ba7b6
Tobi <tobipristupin@gmail.com>
2018-06-01 16:56:05 -0400

    Akcija Python3

--- akcija_25.py
+++ akcija_25.py
@@ -3,12 +3,15 @@ import sys
 books = []
 total = 0
 
-books = sorted(map(int, sys.stdin.read().split("\n")[1:]), reverse=True)
+for linenum, line in enumerate(sys.stdin):
+    if linenum == 0 :
+        continue
+    books.append(int(line))
+
+books.sort(reverse=True)
 
 for i in range(len(books)) :
-	if  (i + 1) % 3 != 0 :
-		total += books[i]
+    if  (i + 1) % 3 != 0 :
+        total += books[i]
 
 print(total)
-
-
