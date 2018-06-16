76e9240381c6227062699fda80cc4289ae3deb5a
Tobi <tobipristupin@gmail.com>
2018-06-16 18:23:48 -0400

    Guess The Data Structure 2.9

---
+++ guessthedatastructure_29.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def main():
+    queue = deque()
+    stack = deque()
+    priority = []
+    valid = {"queue", "stack", "priority queue"}
+
+    for _ in range(int(input())):
+        operation, number = map(int, input().split())
+        if operation == 1:
+            queue.append(number) if "queue" in valid else None
+            stack.appendleft(number) if "stack" in valid else None
+            priority.append(number) ; priority.sort(reverse=True) if "priority queue" in valid else None
+        else:
+            try :
+                valid.discard("queue") if "queue" in valid and queue.popleft() != number else None
+                valid.discard("stack") if "stack" in valid and stack.popleft() != number else None
+                valid.discard("priority queue") if "priority queue" in valid and priority.pop(0) != number else None
+            except IndexError:  # Getting more items than added, this data structure doesn't exist
+                valid.clear()
+                break
+
+    if len(valid) == 0:
+        print("impossible")
+    elif len(valid) == 1:
+        print(valid.pop())
+    else:
+        print("not sure")
+    
+
+while True:
+    try:
+        main()
+    except:
+        break
