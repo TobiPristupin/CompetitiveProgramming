62a1d49fb980c65953a5bf1f6a1f7aacaf2acb45
Tobi <tobipristupin@gmail.com>
2018-07-11 12:18:08 -0400

    Renamed

--- artichoke.py
+++ artichoke.py
@@ -11,12 +11,11 @@ def price(k):
 	return (p * (math.sin(a * k + b) + math.cos(c * k + d) + 2))
 
 prices = [price(i) for i in range(1, n + 1)]
-
 max_decline = 0
 max_price = prices[0]
 
 for i in prices[1:]:
-	i = max(max_price, i)
+	max_price = max(max_price, i)
 	max_decline = max(max_decline, max_price - i)
 
 print(max_decline)
